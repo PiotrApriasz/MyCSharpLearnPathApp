3d0edd972b13580bac38cb7c48c0a3fcd292d39c
Piotr Apriasz <piotrekapriasz@gmail.com>
2021-02-14 04:31:54 +0100

    First idea about User class

---
+++ ApplicationCore/User.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace ApplicationCore
+{
+    public class User
+    {
+        public string Login { get; set; }
+        public string Password { get; set; }
+
+        /// <summary>
+        /// Function to login user to system
+        /// </summary>
+        /// <returns></returns>
+        public bool ToLogIn()
+        {
+            return true;
+        }
+
+        /// <summary>
+        /// Function To register user to the system
+        /// </summary>
+        /// <returns></returns>
+        public bool ToRegister()
+        {
+            return false;
+        }
+    }
+}
