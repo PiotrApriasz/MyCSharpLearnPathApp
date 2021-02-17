57d1a9224aac7b9928c9205e4c72de1ac16b4c6c
Piotr Apriasz <piotrekapriasz@gmail.com>
2021-02-17 15:06:31 +0100

    Add function in LoginService class to perform login

--- DataAccess/LoggingService.cs
+++ DataAccess/LoggingService.cs
@@ -1,9 +1,44 @@
 ﻿using System;
+using ApplicationCore;
+using System.Xml;
+using System.IO;
 
 namespace DataAccess
 {
-    public class LoggingService
+    public static class LoggingService
     {
-        
+        /// <summary>
+        /// Sign in user to his account
+        /// </summary>
+        /// <param name="login">Login that user entered</param>
+        /// <param name="password">Password that user entered</param>
+        /// <returns></returns>
+        public static bool LoginXml(string  login, string password)
+        {
+            bool foundLogin = false;
+            bool foundPassword = false;
+            
+            var xdoc = new XmlDocument();
+            var up = new FileStream("/Database/CSharpLearnPathData.xml", FileMode.Open);
+            xdoc.Load(up);
+            XmlNodeList list = xdoc.GetElementsByTagName("User");
+
+            for (int i = 0; i < list.Count; i++) 
+            {
+                XmlElement cu = (XmlElement) xdoc.GetElementsByTagName("User")[i];
+
+                if (cu?.GetAttribute("Login") == login)
+                {
+                    foundLogin = true;
+                    
+                    XmlElement pass = (XmlElement) xdoc.GetElementsByTagName("Password")[i];
+
+                    if (pass?.InnerText == password) foundPassword = true;
+                }
+            }
+            up.Close();
+
+            return foundLogin && foundPassword;
+        }
     }
 }
---
+++ DataAccessTest/LoginTests.cs
@@ -0,0 +1,15 @@
+﻿using DataAccess;
+using NUnit.Framework;
+
+namespace DataAccessTest
+{
+    public class LoginTests
+    {
+        [Test]
+        public void LoginXml_Test()
+        {
+            var actual = LoggingService.LoginXml("piotrek", "placki");
+            Assert.IsTrue(actual);
+        }
+    }
+}
