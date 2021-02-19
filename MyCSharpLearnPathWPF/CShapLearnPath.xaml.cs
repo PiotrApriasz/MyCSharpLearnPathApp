11516f8222a4b7c28b0f13409f91d65cc0e407ea
Piotr Apriasz <piotrekapriasz@gmail.com>
2021-02-19 02:56:05 +0100

    Set up button pointers behaviour

--- MyCSharpLearnPathWPF/CShapLearnPath.xaml.cs
+++ MyCSharpLearnPathWPF/CShapLearnPath.xaml.cs
@@ -1,5 +1,7 @@
 ﻿using System.Windows;
+using System.Windows.Media;
 using ApplicationCore;
+using DataAccess;
 using MyCSharpLearnPathWPF.Views;
 
 namespace MyCSharpLearnPathWPF
@@ -15,6 +17,9 @@ namespace MyCSharpLearnPathWPF
         {
             User = user;
             DataContext = new Home();
+            Pointer3.Opacity = 0;
+            Pointer2.Opacity = 0;
+            Pointer1.Opacity = 100;
         }
         
         private User User { get; set; }
@@ -22,16 +27,26 @@ namespace MyCSharpLearnPathWPF
         private void HomeButton_OnClick(object sender, RoutedEventArgs e)
         {
             DataContext = new Home();
+            Pointer3.Opacity = 0;
+            Pointer2.Opacity = 0;
+            Pointer1.Opacity = 100;
         }
 
         private void LearnPathButton_OnClick(object sender, RoutedEventArgs e)
         {
             DataContext = new LearnPath();
+            Pointer1.Opacity = 0;
+            Pointer3.Opacity = 0;
+            Pointer2.Opacity = 100;
+            
         }
 
         private void ReminderButton_OnClick(object sender, RoutedEventArgs e)
         {
             DataContext = new Reminder();
+            Pointer1.Opacity = 0;
+            Pointer2.Opacity = 0;
+            Pointer3.Opacity = 100;
         }
     }
 }
